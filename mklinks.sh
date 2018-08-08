062e4dc7e4ddc737e2d878575e43106dc2ef3ce0
mahesh1975 <42197400+mahesh1975@users.noreply.github.com>
2018-08-08 10:39:22 +0530

    dfgdfgdf
    sdgdfgdgf

---
+++ mklinks.sh
@@ -0,0 +1,38 @@
+##
+## Script to make links to the relevant eps figures from the simulation directory
+##
+rm deg00.eps deg45.eps deg90.eps
+##
+ln -s ~/students/niraj_nayan/nn40/script/figures/deg00.eps . 
+ln -s ~/students/niraj_nayan/nn40/script/figures/deg45.eps . 
+ln -s ~/students/niraj_nayan/nn40/script/figures/deg90.eps . 
+##
+rm pow00.eps pow45.eps pow90.eps
+##
+ln -s ~/students/niraj_nayan/nn40/script/figures/pow00.eps .
+ln -s ~/students/niraj_nayan/nn40/script/figures/pow45.eps .
+ln -s ~/students/niraj_nayan/nn40/script/figures/pow90.eps .
+##
+rm pa_ys00.eps pa_ys45.eps pa_ys90.eps
+##
+ln -s ~/students/niraj_nayan/nn40/script/figures/pa_ys00.eps .
+ln -s ~/students/niraj_nayan/nn40/script/figures/pa_ys45.eps .
+ln -s ~/students/niraj_nayan/nn40/script/figures/pa_ys90.eps .
+##
+rm cmp00.eps cmp45.eps cmp90.eps
+##
+ln -s ~/students/niraj_nayan/nn40/script/figures/cmp00.eps . 
+ln -s ~/students/niraj_nayan/nn40/script/figures/cmp45.eps . 
+ln -s ~/students/niraj_nayan/nn40/script/figures/cmp90.eps . 
+##
+rm prestex.eps brasstex.eps randtex.eps
+ln -s ~/students/niraj_nayan/nn40/script/figures/prestex.eps . 
+ln -s ~/students/niraj_nayan/nn40/script/figures/randtex.eps . 
+ln -s ~/students/niraj_nayan/nn40/script/figures/brasstex.eps . 
+##
+rm yieldpoint.eps hardening.eps
+ln -s ~/students/niraj_nayan/nn40/script/figures/yieldpoint.eps . 
+ln -s ~/students/niraj_nayan/nn40/script/figures/hardening.eps . 
+##
+rm coplanar.eps
+ln -s ~/students/niraj_nayan/nn41/script/figures/coplanar.eps . 
